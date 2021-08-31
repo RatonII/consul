db3476d07c0e5151baf366fcb4237f5cf0820acc
Marius Stanescu <mariuss2007@gmail.com>
2021-08-31 17:43:41 +0300

    Adding test jenkinsfile

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export PGPASSWORD=$(awk '/password/ {print $2}' config/database.yml)
+export PGPASSWORD=$(awk '/password/ {print $2}' config/database.yml | cut -d'"' -f 2)
 set -x
 DATABASE=$(psql -lqt -h localhost -U postgres | awk '/consul/ {print $1}')
 if [ -z $DATABASE ]; then
